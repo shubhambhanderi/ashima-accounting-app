a3b9b1ce8dbdea6f8c9d7230f67214a3e4e4d97f
Shubham Bhanderi <sbhanderi18@gmail.com>
2020-08-18 13:49:09 +0530

    api-wp

---
+++ server/src/api/as2020.js
@@ -0,0 +1,35 @@
+/* eslint-disable prefer-arrow-callback */
+/* eslint-disable no-console */
+/* eslint-disable prefer-destructuring */
+/* eslint-disable linebreak-style */
+/* eslint-disable quotes */
+/* eslint-disable linebreak-style */
+/* eslint-disable spaced-comment */
+/* eslint-disable no-unused-vars */
+const express = require('express');
+// const MongoClient = require('mongodb').MongoClient;
+const monk = require('monk');
+// const Joi = require('@hapi/joi');
+
+const db = monk(process.env.MONGO_URI);
+db.then(() => {
+  console.log('Connected correctly to server');
+});
+const REPO3 = db.get('REPO3');
+
+const router = express.Router();
+
+//Read ALL
+router.get('/listofparties', async (req, res, next) => {
+  try {
+    const items = await REPO3.find(
+      {},
+      { partyName: true },
+    );
+    res.json(items);
+  } catch (error) {
+    next(error);
+  }
+});
+
+module.exports = router;
--- server/src/api/index.js
+++ server/src/api/index.js
@@ -3,6 +3,7 @@ const express = require('express');
 
 const emojis = require('./emojis');
 const faqs = require('./faqs');
+const as2020 = require('./as2020');
 
 const router = express.Router();
 
@@ -14,5 +15,6 @@ router.get('/', (req, res) => {
 
 router.use('/emojis', emojis);
 router.use('/faqs', faqs);
+router.use('/as2020', as2020);
 
 module.exports = router;
