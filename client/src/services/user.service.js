37bc4ba810ad12657d41ff6cd8a504d2a73d86b3
Shubham Bhanderi <sbhanderi18@gmail.com>
2020-09-11 10:01:58 +0530

    bug-fixed

--- client/src/services/user.service.js
+++ client/src/services/user.service.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 import authHeader from "./auth-header";
-//s://ashima-accounting-node.herokuapp.com
-const API_URL = "http://localhost:5000/api/";
+const API_URL = "https://ashima-accounting-node.herokuapp.com/api/";
 const API_uri = "https://temp-testing-app.herokuapp.com/";
 
 const getPartylist = () => {
